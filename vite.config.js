65e35102deed90fbdcd4c422fc4408c296fce92f
Oleksandr Patarykin <patter1989@gmail.com>
2025-01-23 00:28:39 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-// import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // // https://vitejs.dev/config/
-// export default defineConfig({
-// 	plugins: [react()],
-// 	build: {
-// 		sourcemap: true,
-// 	},
-// });
-
-
-import { defineConfig } from "vite";
-
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "https://maps.googleapis.com",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
+	build: {
+		sourcemap: true,
 	},
 });
+
+
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		proxy: {
+// 			"/api": {
+// 				target: "https://maps.googleapis.com",
+// 				changeOrigin: true,
+// 				rewrite: (path) => path.replace(/^\/api/, ""),
+// 			},
+// 		},
+// 	},
+// });
