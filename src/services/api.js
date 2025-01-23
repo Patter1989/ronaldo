0fd71cfc465cb7cd13537f2d6375b46dfe725a21
Oleksandr Patarykin <patter1989@gmail.com>
2025-01-23 10:48:38 +0100

    proxy deleted

--- src/services/api.js
+++ src/services/api.js
@@ -5,9 +5,11 @@ const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 export const fetchPlaceDetails = async () => {
 	try {
 		
-		const url = `/api/maps/api/place/details/json?place_id=ChIJe_GwfBUXvUcRLhqeCSgOs1k&fields=name,rating,reviews&key=${GOOGLE_API_KEY}&timestamp=${new Date().getTime()}`;
-		const response = await axios.get(url);
+		// const url = `/api/maps/api/place/details/json?place_id=ChIJe_GwfBUXvUcRLhqeCSgOs1k&fields=name,rating,reviews&key=${GOOGLE_API_KEY}&timestamp=${new Date().getTime()}`;
+				const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJe_GwfBUXvUcRLhqeCSgOs1k&fields=name,rating,reviews&key=${GOOGLE_API_KEY}&timestamp=${new Date().getTime()}`;
 
+		const response = await axios.get(url);
+		console.log(response);
 		// Перевіряємо статус відповіді
 		if (response.data.status !== "OK") {
 			throw new Error(`Error fetching place details: ${response.data.status}`);
--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // // https://vitejs.dev/config/
-// export default defineConfig({
-// 	plugins: [react()],
-// 	build: {
-// 		sourcemap: true,
-// 	},
-// });
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		sourcemap: true,
+	},
+});
 
 
 // import { defineConfig } from "vite";
 
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "https://maps.googleapis.com",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
-});
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		proxy: {
+// 			"/api": {
+// 				target: "https://maps.googleapis.com",
+// 				changeOrigin: true,
+// 				rewrite: (path) => path.replace(/^\/api/, ""),
+// 			},
+// 		},
+// 	},
+// });
